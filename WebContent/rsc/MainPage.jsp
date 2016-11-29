a9f5aada3a322fbdd63c2919edb717da2b003d4a
minesotha <minesotha@gmail.com>
2016-11-29 20:49:50 +0100

    NAPRAWIONE

---
+++ WebContent/rsc/MainPage.jsp
@@ -0,0 +1,27 @@
+<%@page import="java.util.Date"%>
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<script src="js/map.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="css/map.css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta name="google-signin-client_id" content="264956101516-q0ur3n03vqjcr2re05roe2h06300u91e.apps.googleusercontent.com">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<title>Photo World!</title>
+</head>
+<body>
+  <h3>Photo World Demo</h3> 
+  <img id="avatar"></img>
+  <label id="coords"></label>
+  <div class="g-signin2" data-onsuccess="onSignIn"></div>
+<a href="#" onclick="signOut()">Sign out</a>
+    <div id="map"></div>
+  
+  <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJAI8YZkwjydpg2u3WHsZR4G8CkYlZwbA&callback=initMap">
+    </script>  
+</body>
+</html>
---
+++ WebContent/rsc/js/map.js
@@ -0,0 +1,60 @@
+var map; 
+var infowindow;
+
+function initMap() {
+        var centerPoint = {lat: 52.0, lng: 19.0};
+        	map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          disableDoubleClickZoom: true,
+          center: centerPoint
+        });
+        
+        
+        map.addListener( 'mousemove', function (event) {
+        	displayCoordinates(event.latLng);               
+        });
+
+        map.addListener( 'dblclick', function (event) {
+        	addMarker(event.latLng);               
+        });
+        
+        infowindow = new google.maps.InfoWindow();
+}
+
+function onSignIn(googleUser) {
+	  var profile = googleUser.getBasicProfile();
+	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	  console.log('Name: ' + profile.getName());
+	  console.log('Image URL: ' + profile.getImageUrl());
+	  console.log('Email: ' + profile.getEmail());
+	  $("#avatar").attr("src", profile.getImageUrl());
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+
+
+function displayCoordinates(pnt) {
+var lat = pnt.lat();
+lat = lat.toFixed(4);
+var lng = pnt.lng();
+lng = lng.toFixed(4);
+$("#coords").text("Latitude: " + lat + "  Longitude: " + lng);
+}
+
+function addMarker(pnt){  
+    var marker = new google.maps.Marker({
+      position: pnt,
+      map: map
+    });
+    marker.addListener('click', function() {
+    	//testowo ustawianie kontentu na długość i szerokość, w przyszłości obrazek
+    	infowindow.setContent("LAT: "+marker.position.lat().toFixed(2) + "\t LON: "+marker.position.lng().toFixed(2));
+        infowindow.open(map, marker);
+      });
+}
