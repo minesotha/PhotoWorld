af312ab97cfff819d9b5a8aaa75ebe59c17f5e8d
minesotha <minesotha@gmail.com>
2016-11-29 20:55:57 +0100

    servlet podpięty pod stronę

--- WebContent/rsc/MainPage.jsp
+++ WebContent/rsc/MainPage.jsp
@@ -1,6 +1,8 @@
 <%@page import="java.util.Date"%>
 <%@ page language="java" contentType="text/html; charset=US-ASCII"
     pageEncoding="US-ASCII"%>
+        <!-- zignorować bład xD -->
+    <jsp:include page="/servlet" /> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
--- src/com/photoworld/main/MainServlet.java
+++ src/com/photoworld/main/MainServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet("/MainServlet")
+@WebServlet("/servlet")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +27,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("test dodawania servletu").append(request.getContextPath());
 	}
 
 	/**
