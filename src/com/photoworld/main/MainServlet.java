65de011db8a576032429f670de25c0565cdc2c33
minesotha <minesotha@gmail.com>
2016-12-14 09:20:35 +0100

    poprawka po merge

--- src/com/photoworld/main/MainServlet.java
+++ src/com/photoworld/main/MainServlet.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
