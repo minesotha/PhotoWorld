2479ddd4fa281bf074b727bfacd735f246d71c18
minesotha <minesotha@gmail.com>
2017-01-15 22:46:13 +0100

    poprawka servleta

--- src/com/photoworld/main/MainServlet.java
+++ src/com/photoworld/main/MainServlet.java
@@ -28,7 +28,6 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 /**
  * Servlet implementation class MainServlet
  */
-<<<<<<< HEAD
 
 @MultipartConfig(maxFileSize = 16177215)
 public class MainServlet extends HttpServlet {
@@ -172,92 +171,5 @@ public class MainServlet extends HttpServlet {
 		}
 
 	}
-=======
-@WebServlet("/servlet")
-@MultipartConfig(maxFileSize = 16177215) 
-public class MainServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
- // database connection settings
-    private String dbURL = "jdbc:mysql://localhost:3306/photoworld";
-    private String dbUser = "root";
-    private String dbPass = "admin";
-     
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        // gets values of text fields
-    	
-    	//naprawiæ
-        //String username = request.getParameter("google-signin-client_id");
-    	String username = "helen";
-    	
-       
-         
-        InputStream inputStream = null; // input stream of the upload file
-         
-        // obtains the upload file part in this multipart request
-        Part filePart = request.getPart("photo");
-        if (filePart != null) {
-            // prints out some information for debugging
-            System.out.println(filePart.getName());
-            System.out.println(filePart.getSize());
-            System.out.println(filePart.getContentType());
-             
-            // obtains input stream of the upload file
-            inputStream = filePart.getInputStream();
-        }
-         
-        Connection conn = null; // connection to the database
-        String message = null;  // message will be sent back to client
-         
-        try {
-            // connects to the database
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
- 
-            // constructs SQL statement
-            String sql = "INSERT INTO data (username, photo) values (?, ?)";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setString(1, username);
-            
-             
-            if (inputStream != null) {
-                // fetches input stream of the upload file for the blob column
-                statement.setBlob(2, inputStream);
-            }
- 
-            // sends the statement to the database server
-            int row = statement.executeUpdate();
-            if (row > 0) {
-                message = "File uploaded and saved into database";
-            }
-        } catch (SQLException ex) {
-            message = "ERROR: " + ex.getMessage();
-            ex.printStackTrace();
-        } finally {
-            if (conn != null) {
-                // closes the database connection
-                try {
-                    conn.close();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            // sets the message in request scope
-            request.setAttribute("Message", message);
-             
-            // forwards to the message page
-            getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
-        }
-    }
->>>>>>> parent of 9e2293d... mapowanie rsc
 
 }
